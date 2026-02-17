436cdfb9bdb0ba4fd9543bf1be5f468d3cf6930a
neeteshdixit <dixitneetesh857@gmail.com>
2026-02-17 12:14:37 +0530

    datatype

---
+++ javascript/datatype.js
@@ -0,0 +1,18 @@
+// // console.log(10+20)
+// let a = 30  // block-scope hai, redeclare not possible
+// let b = 50
+
+// console.log(a+b)
+// console.log(d)
+// var d = 34   // var ko functional scope bolne hai we can acess before assigned
+// var d = 34
+// console.log(d)
+
+
+// let let_var = "let variable"
+
+// temparor dead zone
+
+// undefined
+let test 
+console.log(test)
